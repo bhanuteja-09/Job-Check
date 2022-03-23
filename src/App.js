99d04c6458cc694a3a0f1dfe3b985d2bdf95f417
Vinay Mamidisett <Vinay.Mamidisett@Pratian.com>
2022-03-23 10:08:36 +0530

    searchSubscription

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchSubscription from './Modules/Admin/Subscriptions/Pages/SearchSubscription/SearchSubscriptions';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchSubscription/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
