acb3dce9e488167926943fbf65fa13ae44686bf3
Vinay Mamidisett <Vinay.Mamidisett@Pratian.com>
2022-03-23 12:07:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import AddSubscription from './Modules/Admin/Subscriptions/Pages/AddSubscription/AddSubscription';
-import SearchSubscription from './Modules/Admin/Subscriptions/Pages/SearchSubscription/SearchSubscriptions';
+
 
 
 const App = () => {
   return (
     <div>
-      <>
-      <SearchSubscription/>
-      {/* <AddSubscription/> */}
-      </>
      
     </div>
   )
