02160d9964cbe386dac6436ba9a6ec6adcebb4c5
Rohith_Kumar <Rohith.Kumar@skillassure.com>
2022-04-21 10:09:46 +0530

    updated

--- src/Components/Shared/SideBar.js
+++ src/Components/Shared/SideBar.js
@@ -1,110 +1,116 @@
-import React from 'react'
-import "../../Assets/Styles/sidebar.css"
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import DynamicFeedOutlinedIcon from '@mui/icons-material/DynamicFeedOutlined';
-import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
-import EqualizerOutlinedIcon from '@mui/icons-material/EqualizerOutlined';
-import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
-import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
-import { Box,List,ListItem,ListItemText,ListItemButton,ListItemIcon } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { Typography } from '@mui/material';
-import AddCardOutlinedIcon from '@mui/icons-material/AddCardOutlined';
-
-
-
+import React from "react";
+import "../../Assets/Styles/sidebar.css";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import DynamicFeedOutlinedIcon from "@mui/icons-material/DynamicFeedOutlined";
+import AssignmentTurnedInOutlinedIcon from "@mui/icons-material/AssignmentTurnedInOutlined";
+import EqualizerOutlinedIcon from "@mui/icons-material/EqualizerOutlined";
+import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
+import DoneAllOutlinedIcon from "@mui/icons-material/DoneAllOutlined";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  ListItemIcon,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Typography } from "@mui/material";
+import AddCardOutlinedIcon from "@mui/icons-material/AddCardOutlined";
 
 const SiderBar = () => {
-    let Navigate = useNavigate()
-    
+  let Navigate = useNavigate();
+
   return (
     <div>
-      
-  <nav className="main-menu">
-      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <nav aria-label="main mailbox folders">
-          <List> 
-              <ListItem disablePadding >
-            <ListItemButton>
-              <ListItemIcon >
-              <DoneAllOutlinedIcon sx={{color:"green"}} />
-              </ListItemIcon>
-              <ListItemText  onClick={()=>Navigate("/")}>
-                <Typography variant='h5'>Job Check</Typography>
-                </ListItemText>
-            </ListItemButton>
-          </ListItem></List>
-      
-     
-        <List sx={{marginTop:5}}>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              <HomeOutlinedIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Home" onClick={()=>Navigate()} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <SearchOutlinedIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Profile Search" onClick={()=>Navigate()} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <DynamicFeedOutlinedIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Job Post" onClick={()=>Navigate("/JobPost")} />
-            </ListItemButton>
-          </ListItem>
-        
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <AssignmentTurnedInOutlinedIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Requirement" onClick={()=>Navigate("/Requirements")} />
-            </ListItemButton>
-          </ListItem>
-        
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <EqualizerOutlinedIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Analytics" onClick={()=>Navigate()} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <GroupOutlinedIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Users" onClick={()=>Navigate()} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <AddCardOutlinedIcon/>
-              </ListItemIcon>
-              <ListItemText primary="Subscriptions" onClick={()=>Navigate("/SearchSubscription")} />
-            </ListItemButton>
-          </ListItem>
-        </List>
+      <nav className="main-menu">
+        <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItem disablePadding onClick={() => Navigate("/")}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <DoneAllOutlinedIcon sx={{ color: "green" }} />
+                  </ListItemIcon>
+                  <ListItemText>
+                    <Typography variant="h5">Job Check</Typography>
+                  </ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </List>
+
+            <List sx={{ marginTop: 5 }}>
+              <ListItem disablePadding onClick={() => Navigate()}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <HomeOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding onClick={() => Navigate()}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <SearchOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Profile Search" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding onClick={() => Navigate("/JobPost")}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <DynamicFeedOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Job Post" />
+                </ListItemButton>
+              </ListItem>
+
+              <ListItem
+                disablePadding
+                onClick={() => Navigate("/Requirements")}
+              >
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AssignmentTurnedInOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Requirement" />
+                </ListItemButton>
+              </ListItem>
+
+              <ListItem disablePadding onClick={() => Navigate()}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <EqualizerOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Analytics" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding onClick={() => Navigate()}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <GroupOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Users" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem
+                disablePadding
+                onClick={() => Navigate("/SearchSubscription")}
+              >
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AddCardOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Subscriptions" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Box>
       </nav>
-    </Box>
-
-           
-        </nav>
-  
     </div>
-  )
-}
+  );
+};
 
-export default SiderBar
+export default SiderBar;
