8a1f92faab410a548cce54f26a1f86fbf1922329
Bhanu Tejaswini Kaluva <bhanutejaswinikaluva@gmail.com>
2023-11-01 16:31:41 +0530

    Second Commit

--- src/Modules/Recruitment/JobPosts/Pages/SearchJobPost/SearchJobPosts.jsx
+++ src/Modules/Recruitment/JobPosts/Pages/SearchJobPost/SearchJobPosts.jsx
@@ -16,7 +16,7 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import LastPageIcon from "@mui/icons-material/LastPage";
 import { useSelector, useDispatch } from "react-redux";
-import { loadJobPosts, sortJobposts } from "../Redux/Actions/actions";
+import { loadJobPosts, sortJobPost } from "../Redux/Actions/actions";
 import { useNavigate } from "react-router-dom";
 import EditIcon from "@mui/icons-material/Edit";
 import SearchIcon from "@mui/icons-material/Search";
@@ -140,7 +140,7 @@ const SearchJobPosts = () => {
 
 
   const handleSort = (value) => {
-    dispatch(sortJobposts(value));
+    dispatch(sortJobPost(value));
   };
 
   const handleChangeRowsPerPage = (event) => {
